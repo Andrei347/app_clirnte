c3ee2e460482a220493abceb4c46b240aa0af73c
Andrei <6364650@alu.murciaeduca.es>
2019-05-14 22:51:47 +0200

    error v-for en el index

--- js/codigo.js
+++ js/codigo.js
@@ -1,20 +1,22 @@
-var app = new Vue({
+new Vue({
     el: '#app',
-    mounted() {
-       this.traerdatos();
-    },
     data: {
-
+        todo: [],
+    }, mounted() {
+        this.traerdatos();
     },
-    methods:{
+    methods: {
         showmoreid: function () {
-            window.location="show_more.html";
+            window.location = "show_more.html";
         },
         traerdatos: function () {
-            axios.put('http://127.0.0.1:8000/api/reqbudget/5')
+            axios.get('http://127.0.0.1:8000/api/reqbudget/')
                 .then((respuesta) => {
-                    console.log(respuesta);
-                    //this.personas=respuesta.data.results;
+                    console.log(respuesta.data.data);
+                    for (var i = 0; i <respuesta.data.data.length ; i++) {
+                        this.todo[i]=respuesta.data.data[i];
+                    }
+                    console.log(this.todo);
                 });
         }
     }
