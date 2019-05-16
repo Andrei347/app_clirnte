d3637ca58554d0a1425ce323aaa5e6b207816488
Andrei <6364650@alu.murciaeduca.es>
2019-05-16 22:03:42 +0200

    16-05-19-2

--- js/codigo.js
+++ js/codigo.js
@@ -1,23 +1,38 @@
 new Vue({
     el: '#app',
     data: {
+        id: "",
+        title: "",
+        description: "",
+        category_id: "",
+        user_id: "",
+        updated_at: "",
+        state: "",
         todo: [],
+        especial: [],
     }, mounted() {
         this.traerdatos();
     },
     methods: {
         showmoreid: function (id) {
-            console.log(id);
-
             window.location = "show_more.html";
+            console.log(id);
+            console.log(this.todo[id].description);
+            this.title = this.todo[id].title;
+            this.description = this.todo[id].description;
+            this.category_id = this.todo[id].category_id;
+            this.user_id = this.todo[id].user_id;
+            this.state = this.todo[id].state;
+            this.updated_at = this.todo[id].updated_at;
+           // window.location = "show_more.html";
         },
         traerdatos: function () {
             axios.get('http://127.0.0.1:8000/api/reqbudget/')
                 .then((respuesta) => {
                     //console.log(respuesta.data.data);
-                        this.todo=respuesta.data.data;
+                    this.todo = respuesta.data.data;
                     console.log(this.todo);
                 });
         }
     }
-})
+});
