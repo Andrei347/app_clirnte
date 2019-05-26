ae9a4d92f49ea93fe51ce8ec60c2e931b17fecd4
Andrei <6364650@alu.murciaeduca.es>
2019-05-26 21:06:38 +0200

    user y mas

--- js/codigo.js
+++ js/codigo.js
@@ -1,31 +1,34 @@
 new Vue({
     el: '#app',
     data: {
-        id: "",
+        username:"",
+        password:"",
         title: "",
         description: "",
         category_id: "",
         user_id: "",
         updated_at: "",
         state: "",
+        image:"",
         todo: [],
-        especial: [],
     }, mounted() {
         this.traerdatos();
     },
     methods: {
         showmoreid: function (id) {
+            console.log(this.todo[id]);
             window.location = "show_more.html";
-            console.log(id);
-            console.log(this.todo[id].description);
+            this.mostrar(id);
+        },
+        mostrar:function(id){
             this.title = this.todo[id].title;
             this.description = this.todo[id].description;
             this.category_id = this.todo[id].category_id;
             this.user_id = this.todo[id].user_id;
             this.state = this.todo[id].state;
             this.updated_at = this.todo[id].updated_at;
-           // window.location = "show_more.html";
         },
+
         traerdatos: function () {
             axios.get('http://127.0.0.1:8000/api/reqbudget/')
                 .then((respuesta) => {
@@ -33,6 +36,10 @@ new Vue({
                     this.todo = respuesta.data.data;
                     console.log(this.todo);
                 });
+        },
+        sesion: function () {
+            console.log(this.username, this.password);
+            //window.location = "inicio.html";
         }
     }
 });
